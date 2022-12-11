f3b6acdd0506e0c10402d2fed1bd78b6df8d7b50
James Camacho <camacho.james@gmail.com>
2022-12-10 19:03:42 -0500

    Added setup file to download word data.

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p data
+
+echo "Downloading words..."
+curl https://raw.githubusercontent.com/taikuukaits/SimpleWordlists/master/Wordlist-Adjectives-All.txt -o data/words.txt
+curl https://raw.githubusercontent.com/taikuukaits/SimpleWordlists/master/Wordlist-Adverbs-All.txt >> data/words.txt
+curl https://raw.githubusercontent.com/taikuukaits/SimpleWordlists/master/Wordlist-Nouns-All.txt >> data/words.txt
+curl https://raw.githubusercontent.com/taikuukaits/SimpleWordlists/master/Wordlist-Verbs-All.txt >> data/words.txt
+
+echo "Sorting words..."
+sort -o data/words.txt{,}
+
+echo "Downloading antonyms..."
+curl https://raw.githubusercontent.com/taikuukaits/SimpleWordlists/master/Thesaurus-Antonyms-All.txt -o data/antonyms.txt
+
+echo "Downloading synonyms..."
+curl https://raw.githubusercontent.com/taikuukaits/SimpleWordlists/master/Thesaurus-Synonyms-All.txt -o data/synonyms.txt
